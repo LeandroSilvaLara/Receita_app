0cab3ee40e2196440d86cab1db0d9aa4a977d407
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2026-01-02 23:57:44 -0300

    feat: Create reusable authentication button component
    This commit introduces a new reusable Jetpack Compose component, `AuthButton`, designed for authentication screens like login and registration.
    
    - **`AuthButton.kt`:**
      - A new composable function `AuthButton` is created to provide a standardized button style for authentication forms.
      - It accepts parameters for text, loading state, content color, and an enabled flag.
      - When `isLoading` is true, it displays a `CircularProgressIndicator` instead of text.
      - The button has a `RoundedCornerShape` and uses colors from the `MaterialTheme`.
      - A disabled state with a light gray background color is also defined.
      - A `@Preview` is included to visualize both the standard and loading states of the button.

---
+++ app/src/main/java/com/courselara/receitafacil/ui/presentation/components/button/AuthButton.kt
@@ -0,0 +1,68 @@
+package com.courselara.receitafacil.ui.presentation.components.button
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.courselara.receitafacil.ui.theme.ReceitaFacilAppTheme
+import com.courselara.receitafacil.ui.theme.poppinsFOntFamily
+
+@Composable
+fun AuthButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    isLoading: Boolean,
+    contentColor: Color,
+    enabled: Boolean= true,
+    onButtonClick: () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        onClick = onButtonClick,
+        shape = RoundedCornerShape(25.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.White,
+            disabledContainerColor = Color.LightGray
+        ),
+        enabled = enabled
+    ) {
+        if (isLoading) {
+            CircularProgressIndicator(
+                color = contentColor,
+                modifier = Modifier.size(20.dp)
+            )
+        } else {
+            Text(
+                text = text,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = poppinsFOntFamily,
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AuthButtonPreview() {
+    ReceitaFacilAppTheme {
+        AuthButton(
+            text = "Login",
+            isLoading = true,
+            contentColor = Color.White,
+            onButtonClick = {}
+        )
+    }
+}
