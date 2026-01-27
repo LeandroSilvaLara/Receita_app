38fb8ebb1711391e2a6a634c9a3bec340b17ed97
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2026-01-26 23:23:58 -0300

    feat: Implement CommonTopBar and BottomBar components
    This commit introduces reusable UI components for navigation and app structure, specifically a customizable top bar and a functional bottom application bar.
    
    - **Presentation Layer (`BottomBar.kt`):**
        - Created a `BottomBar` component using `BottomAppBar` that includes navigation icons for users, search, and profile.
        - Integrated an `ExtendedFloatingActionButton` within the bar to handle the "New Recipe" action.
        - Added a dark mode preview for the component.
    
    - **Presentation Layer (`CommonTopBar.kt`):**
        - Created a `CommonTopBar` component using `TopAppBar` (Material3) to standardize headers across different screens.
        - Added support for dynamic titles, navigation icons (e.g., back button), and action icons (e.g., save button).
        - Implemented state handling for enabled/disabled actions with corresponding tint adjustments.

---
+++ app/src/main/java/com/courselara/receitafacil/ui/presentation/components/bottombar/BottomBar.kt
@@ -0,0 +1,104 @@
+package com.courselara.receitafacil.ui.presentation.components.bottombar
+
+import android.content.res.Configuration
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.PeopleOutline
+import androidx.compose.material.icons.outlined.PersonOutline
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.courselara.receitafacil.R
+import com.courselara.receitafacil.ui.presentation.navigation.screens.AuthScreens
+import com.courselara.receitafacil.ui.theme.ReceitaFacilAppTheme
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier,
+    onNavigateToProfileScreens: () -> Unit,
+    onNavigateToSearchScreen: () -> Unit,
+    onNavigationToAddRecipeScreen: () -> Unit,
+    onNavigateToUsersConnectionScreen: () -> Unit,
+) {
+    BottomAppBar(
+        modifier = modifier,
+        containerColor = MaterialTheme.colorScheme.background,
+        actions = {
+            IconButton(
+                onClick = onNavigateToUsersConnectionScreen
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.PeopleOutline,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+            IconButton(
+                onClick = onNavigateToSearchScreen
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+            IconButton(
+                onClick = onNavigateToProfileScreens
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.PersonOutline,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = onNavigationToAddRecipeScreen,
+                containerColor = MaterialTheme.colorScheme.primary,
+                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+                icon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Add,
+                        tint = Color.White,
+                        contentDescription = "Add"
+                    )
+                },
+                text = {
+                    Text(
+                        text = stringResource(id = R.string.new_recipe_text),
+                        color = Color.White
+                    )
+                }
+            )
+        }
+    )
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun BottomBarPreview() {
+    ReceitaFacilAppTheme {
+        BottomBar(
+            modifier = Modifier,
+            onNavigateToProfileScreens = {},
+            onNavigateToSearchScreen = {},
+            onNavigationToAddRecipeScreen = {},
+            onNavigateToUsersConnectionScreen = {},
+
+        )
+
+    }
+
+}
